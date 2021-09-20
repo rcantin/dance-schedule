0e7166f80d25e2d36f293550413d16f7c77aa635
rcantin <ryan.cantin@coxautoinc.com>
2021-09-20 11:22:33 -0400

    Changed vue.config.js to have new path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vuejs-sandbox/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dance-schedule/" : "/",
 };
